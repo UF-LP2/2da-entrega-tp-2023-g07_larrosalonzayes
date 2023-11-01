69c5002fd1d506243966275e031d9f0e0fc9c13b
MatiasLarrosa <matularrosa555@gmail.com>
2023-10-31 22:21:54 -0300

    main cambiado

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import csv
+
+from Ambulancia import cAmbulancia
+from Color import cColor
+from Consultorio import cConsultorio
+from Enfermero import cEnfermero
+from Hospital import cHospital
+from Medico import cMedico
+from Paciente import cPaciente
+from SalaEspera import cSalaEspera
+
 def main() -> None:
-  print("Hello World")
+	# Inicio del main
+	print()
+
 
 if __name__ == "__main__":
-  main()
+	main()
