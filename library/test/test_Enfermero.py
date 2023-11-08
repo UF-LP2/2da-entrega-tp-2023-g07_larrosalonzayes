f8c52136effed117f586c8e1ab1a57468f5821ae
charizard56 <mrlonzayes@gmail.com>
2023-11-08 19:44:03 -0300

    test de funcion catalogar pacientes andando ok

--- library/test/test_Enfermero.py
+++ library/test/test_Enfermero.py
@@ -2,7 +2,7 @@ import pytest
 
 from Enfermero import cEnfermero
 from Paciente import cPaciente
-from Color import cColor, Colores
+from Color import cColor
 
 #/////////////////////////////////////// TESTEO DE PRUEBA ////////////////////////////////////////////////////// 
 
@@ -17,7 +17,39 @@ def test1():
     # Crea una instancia de cEnfermero y cPacient    
 
 
+def test1_catalogarPaciente():
+
+    enfermero = cEnfermero(12345, "Juan", "Parker", "MANIANA", True)
+    paciente1 = cPaciente(1234, "Clarita", "Lops", "politraumatismo grave", 23, True)
+
+    # Llama al método catalogarPaciente en la instancia de cPaciente
+    paciente1_mod = cEnfermero.catalogarPaciente(paciente1)
+	 # Comprueba si el color del paciente se ha asignado correctamente
+    assert (paciente1_mod.getColoracion()== 5) 
     
+## testo con otra patologia
+    
+def test2_catalogarPaciente():
+
+    enfermero = cEnfermero(1235, "Jackie", "Smith", "TARDE", True)
+    paciente1 = cPaciente(1234, "Rosario", "Lonzayes", "cefalea brusca", 23, True)
+
+    # Llama al método catalogarPaciente en la instancia de cPaciente
+    paciente1_mod = cEnfermero.catalogarPaciente(paciente1)
+	 # Comprueba si el color del paciente se ha asignado correctamente
+    assert (paciente1_mod.getColoracion()== 3)
+
+# Le paso una patologia y arroja false en caso de asignar mal el color
+
+def test3_catalogarPaciente():
+
+    enfermero = cEnfermero(1235, "Jackie", "Smith", "TARDE", True)
+    paciente1 = cPaciente(1234, "Matias", "Larrosa", "hemmoragia digestiva", 22, True)
+
+    # Llama al método catalogarPaciente en la instancia de cPaciente
+    paciente1_mod = cEnfermero.catalogarPaciente(paciente1)
+	 # Comprueba si el color del paciente se ha asignado correctamente
+    assert (paciente1_mod.getColoracion()== 1) == False
 
 
 
